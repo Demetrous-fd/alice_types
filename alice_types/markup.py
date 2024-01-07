f580d50774738186fc8b133e1aee42adb7bd6171
Demetrous-fd <dimaprodeus@yandex.ru>
2024-01-07 21:34:19 +0800

    dangerous_context может содержать значение False

--- alice_types/markup.py
+++ alice_types/markup.py
@@ -6,12 +6,5 @@ from pydantic import BaseModel, Field, model_validator
 class Markup(BaseModel):
     dangerous_context: Optional[bool] = Field(
         default=None,
-        description="Признак реплики, которая содержит криминальный подтекст (самоубийство, разжигание ненависти, угрозы)." \
-                    "Возможно только значение true."
+        description="Признак реплики, которая содержит криминальный подтекст (самоубийство, разжигание ненависти, угрозы)."
     )
-    
-    @model_validator(mode="after") # type: ignore
-    def validate_dangerous_context(self) -> "Markup":
-        if self.dangerous_context is False:
-            raise ValueError("Поле dangerous_context может иметь только значение true. Если признак не применим, это свойство не включается в ответ.")
-        return self
