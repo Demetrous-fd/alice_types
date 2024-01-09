e04cfb43a42367949d3a6dc45adb20ebbf5b4f84
lazydeus <dimaprodeus@yandex.ru>
2024-01-09 09:33:31 +0300

    Remove loguru from dependencies

--- alice_types/logger.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from loguru import logger
-
-logger.add(sys.stdout, format="<green>{time}</green> {level} : {message}", filter="alice_types", level="INFO", enqueue=True)
-logger.add(sys.stderr, format="<red>{time}</red> {level} : {message}", filter="alice_types", level="INFO", enqueue=True)
